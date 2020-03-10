1c53d60cffa998094331c56bede37e811a3c6235
Ricardo199 <ricardo1529@gmail.com>
2020-03-10 11:41:06 -0600

    commit

--- resources/js/empleado.js
+++ resources/js/empleado.js
@@ -2,17 +2,39 @@ $(document).ready(function(){
     $(document).on("click","#cmdEnviar",function(){
         var datos = JSON.stringify($("#frmEmpleado :input").serializeArray());
         //console.log(datos);
-        $.ajax({
-            type: "post",
-            dataType: "JSON",
-            url:"php/recepcion.php",
-            data: {datos:datos}
-        }).done(
-            function(data) {
-               $("#tot").text(data.sueldo);
-        }).fail(
-            function(data){
-
-        });
+         var vacios=validar();
+         if(vacios==0){
+            $.ajax({
+                type: "post",
+                dataType: "JSON",
+                url:"php/recepcion.php",
+                data: {datos:datos}
+            }).done(
+                function(data) {
+                   $("#tot").text(data.sueldo);
+            }).fail(
+                function(data){
+    
+            });
+         }else{
+             alert("no deje espacios requeridos");
+         }        
     });
 });
+function validar(){
+    var cont=0;
+    $(".requerido").each(function(){
+        var x =$(this).val();
+        if(x==""){
+            cont++;
+            $(this).focus();
+            die();            
+        }
+        return cont;
+    });
+}
+
+function die(msg){
+    alert("no deje espacios requeridos");
+    throw msg;
+}
