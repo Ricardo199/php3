ffcb860ad897880c9f9f5aa27942dbf459a40372
Ricardo199 <ricardo1529@gmail.com>
2020-03-10 08:27:39 -0600

    commit

--- resources/js/empleado.js
+++ resources/js/empleado.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    $(document).on("click","#cmdEnviar",function(){
+        var nombres = $("#nombres").val();
+        var apellidos = $("#apellidos").val();
+        var fechanac = $("#fechaNacimiento").val();
+        var sueldo = $("#sueldo").val();
+        var pais = $("#pais").val();
+        console.log(nombres+" "+apellidos+" "+fechanac+" $"+sueldo+" "+pais);
+    });
 });
